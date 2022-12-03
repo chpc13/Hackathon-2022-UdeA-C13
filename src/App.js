1fbbdb02be11a86cfbf6110a7ca7ef9c14d78d20
chpc13 <chpcodigo13@gmail.com>
2022-12-02 19:51:36 -0500

    Hackathon 2022 UdeA

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./components/Tarjetatour/Tarjetatour.jsx"
+import Tarjetatour from './components/Tarjetatour/Tarjetatour.jsx';
+
+
+const App = () => {
+  return (
+    <div>
+      <Tarjetatour />
+    </div>
+  );
+};
+
+export default App
